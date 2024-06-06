e1b1347c665a4efd05447573ee154e7db82dcca2
Gabriel Oliveira <gabriel_lopeslomeu@hotmail.com>
2024-06-06 18:40:51 -0300

    feat: validation to insert an item to the cart

--- src/services/cart-service.ts
+++ src/services/cart-service.ts
@@ -1,6 +1,15 @@
+import { lockedError } from '@/errors/locked-error';
+import { notFoundError } from '@/errors/not-found-error';
 import { cartRepository } from '@/repositories/cart-repository';
+import { listRepository } from '@/repositories/list-itens-repository';
 
 async function insertToCart(body) {
+  const idExists = await listRepository.getItemById(body.itemId);
+  if (!idExists) throw notFoundError('Item não encontrado!');
+  console.log(idExists);
+  if (idExists.quantity <= 0)
+    throw lockedError('item não pode ser selecionado pois já foi reservado por outra pessoa.');
+
   const response = await cartRepository.insertToCart(body);
   return response;
 }
